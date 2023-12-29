386f2ad4ca0e381304145a45840bd80790b9ec22
Christian Noss <christian.noss@th-koeln.de>
2023-12-29 15:17:26 +0100

    ♻️ Pretty up code

--- src/_layouts/components/video.11ty.js
+++ src/_layouts/components/video.11ty.js
@@ -1,26 +1,36 @@
+const getIframe = (url) => {
+  return `
+    <figure class="video info-card-item">
+      <iframe 
+        id="iframeVideo" 
+        class="info-card-video" 
+        src="${url}" 
+        title="YouTube video player" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+      </iframe>
+    </figure>`;
+}
+
+const getVideo = (filename) => {
+  return `
+    <figure class="video info-card-item">
+      <video controls="controls" src="../ar-media/videos/${filename}"></video>
+    </figure>`;
+}
+
 exports.getVideo = (eleventy, data) => {
 
-  if(!data.ar || !data.ar.video) return '';
-  
+  if (!data.ar || !data.ar.video) return '';
+
   const arVideoData = data.ar.video;
 
   const videoData = arVideoData.map(videoElement => {
     const videoContainer = videoElement.type === 'url'
-      ? `<figure class="video info-card-item">
-          <iframe 
-            id="iframeVideo" 
-            class="info-card-video" 
-            src="${videoElement.url}" 
-            title="YouTube video player" 
-            frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-          </iframe>
-        </figure>`
-      : `<figure class="video info-card-item">
-          <video controls="controls" src="../ar-media/videos/${videoElement.filename}"></video>
-        </figure>`;
+      ? getIframe(videoElement.url)
+      : getVideo(videoElement.filename);
 
-      return videoContainer;
+    return videoContainer;
   });
 
   return videoData.join('');
