c973882329c2c591f88a2d55709bc1270b642267
Mario Faske <mario.faske@th-koeln.de>
2024-02-27 10:03:47 +0100

    add logic for mediaControls

--- src/_layouts/threejs_poi.11ty.js
+++ src/_layouts/threejs_poi.11ty.js
@@ -1,11 +1,72 @@
 exports.render = function (data) {
 
-    const { ar } = data;
+  const mediaControls = `
+  <div id="media-controls" class="media-controls" data-js-media-controls>
+    <button id="play-button" data-js-play-button><span class="icon">play_arrow</span></button>
+    <button id="pause-button" class="is-hidden" data-js-pause-button><span class="icon">pause</span></button>
+    <button id="stop-button" class="is-hidden" data-js-stop-button><span class="icon">stop</span></button>
+  </div>
+`;
+
+const mediaControlsLogic = `
+  <script type="module">
+  let audioIsPlaying = false;
+      
+      const addingMediaControls = () => {
+        
+        let playableContent;
+  
+        let playBtn;
+        let stopBtn;
+        let pauseBtn;
+        let mediaControls
+      playableContent = document.getElementById('video');
+  
+      playBtn = document.querySelector("[data-js-play-button]");
+      pauseBtn = document.querySelector("[data-js-pause-button]");
+      stopBtn = document.querySelector("[data-js-stop-button]");
+      mediaControls = document.getElementById("media-controls");
+  
+      mediaControls.classList.add("is-visible");
+  
+      console.log(mediaControls);
+  
+    if(!mediaControls || mediaControls == null) return;
+  
+    const playMedia = () => {
+      playableContent.play();
+      audioIsPlaying = true;
+      playBtn.classList.add("is-hidden");
+      pauseBtn.classList.remove("is-hidden");
+    }
+  
+    const pauseMedia = () => {
+      playableContent.pause();
+      audioIsPlaying = false;
+      playBtn.classList.remove("is-hidden");
+      pauseBtn.classList.add("is-hidden");
+    }
+  
+          mediaControls.addEventListener("click", () => {
+            if(audioIsPlaying){
+                pauseMedia();
+            }else{
+                playMedia();
+            }
+          });
+        }
+        document.addEventListener("DOMContentLoaded", () => {
+          addingMediaControls();
+        })
+  </script>
+`
+
     return `
     <html>
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script async src="https://unpkg.com/es-module-shims@1.8.0/dist/es-module-shims.js"></script>
+    ${mediaControlsLogic}
     <script type="importmap">
     {
       "imports": {
@@ -16,12 +77,17 @@ exports.render = function (data) {
     }
     </script>
     <script type="module">
+
+      // ==============================================================
       import * as THREE from 'three';
       import { MindARThree } from 'mindar-image-three';
       const mindarThree = new MindARThree({
 	      container: document.querySelector("#container"),
 	      imageTargetSrc: "../ar-media/images/pulvermuseum.mind"
       });
+
+      console.log("HALLO");
+
       const {renderer, scene, camera} = mindarThree;
       const anchor = mindarThree.addAnchor(0);
       
@@ -38,7 +104,7 @@ exports.render = function (data) {
       });
       
       function animate(){
-        video.play();
+        //video.play();
         requestAnimationFrame(animate);
         
         videoTexture.needsUpdate = true;
@@ -79,6 +145,47 @@ exports.render = function (data) {
 	left: 0;
 	z-index: 2;
       }
+      .media-controls {
+        opacity: 0;
+        transition: all var(--tr-slow) var(--tr-delay);
+        display: flex;
+        gap: var(--m);
+        z-index: 20;
+        position: absolute;
+        left: 50%;
+        bottom: 10%;
+        transform: translateX(-50%);
+      
+        &.is-visible{
+          opacity: 1;
+        }
+      
+        button {
+          width: var(--button-size);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          font-size: var(--l);
+          aspect-ratio: 1/1;
+          cursor: pointer;
+          border: none;
+          background-color: var(--color-accent);
+          border-radius: 50%;
+          box-shadow: var(--shadow-card);
+      
+          .icon {
+            color: var(--lightest);
+          }
+      
+          &.is-hidden{
+            display: none;
+          }
+        }
+        
+        button:hover {
+          background-color: var(--color-accent);
+        }
+      }
     </style>
   </head>
   <body>
@@ -87,7 +194,6 @@ exports.render = function (data) {
       playsinline
       webkit-playsinline
       loop
-      autoplay
       width="1290"
       height="1040"
       src="../ar-media/videos/pulvermuseum.webm"
@@ -99,6 +205,7 @@ exports.render = function (data) {
     </div>
     <div id="container">
     </div>
+    ${mediaControls}
   </body>
 </html>
     `;
